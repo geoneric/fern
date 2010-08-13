1c2b7eca76c82a560fae06accc8e65b020e4505d
Kor de Jong <kor@jemig.eu>
2010-08-13 09:42:34 +0200

    Added initial Environment tree

---
+++ Environment/Templates/Sources/Class.cc
@@ -0,0 +1,18 @@
+#include "pack_class.h"
+
+
+
+namespace pack {
+
+Class::Class()
+{
+}
+
+
+
+Class::~Class()
+{
+}
+
+} // namespace pack
+
---
+++ Environment/Templates/Sources/Class.h
@@ -0,0 +1,35 @@
+#ifndef INCLUDED_PACK_CLASS
+#define INCLUDED_PACK_CLASS
+
+#include <boost/noncopyable.hpp>
+
+
+
+namespace pack {
+
+//! short_description_HORRIBLE_LONG_STRING_TO_NOTICE_THAT_IT_SHOULD_BE_REPLACED
+/*!
+  longer_description_HORRIBLE_LONG_STRING_TO_NOTICE_THAT_IT_SHOULD_BE_REPLACED
+
+  \sa        .
+*/
+class Class: private boost::noncopyable
+{
+
+  friend class ClassTest;
+
+private:
+
+protected:
+
+public:
+
+                   Class               ();
+
+  /* virtual */    ~Class              ();
+
+};
+
+} // namespace pack
+
+#endif
