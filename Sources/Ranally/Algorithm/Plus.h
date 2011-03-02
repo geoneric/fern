22a7211b7f89ae79f806be89e6c2cf97ab1e30c2
Kor de Jong <kor@jemig.eu>
2011-03-02 21:45:01 +0100

    Busy with template stuff

--- Sources/Ranally/Algorithm/Plus.h
+++ Sources/Ranally/Algorithm/Plus.h
@@ -1,19 +1,179 @@
 #ifndef INCLUDED_RANALLY_ALGORITHM_PLUS
 #define INCLUDED_RANALLY_ALGORITHM_PLUS
 
+#include <boost/range/begin.hpp>
+#include <boost/range/end.hpp>
+#include <boost/range/const_iterator.hpp>
+
 
 
 namespace ranally {
 namespace algorithm {
 namespace detail {
 
+// template<
+//   typename Raster>
+// struct RasterTraits
+// {
+//   static bool const isConstant = false;
+// };
+// 
+// template<>
+// struct RasterTraits<int>
+// {
+//   static bool const isConstant = true;
+// };
 
+
+
+template<
+  typename Argument>
+struct ArgumentTraits
+{
+  static bool const isScalar=false;
+};
+
+
+
+template<>
+struct ArgumentTraits<int>
+{
+  static bool const isScalar=true;
+};
+
+
+
+bool const IS_SCALAR = true;
+bool const IS_NOT_SCALAR = false;
+
+template<
+  typename Argument1,
+  typename Argument2,
+  typename Result,
+  bool argument1IsScalar,
+  bool argument2IsScalar>
+struct Plus
+{
+};
+
+
+
+template<
+  typename Argument1,
+  typename Argument2,
+  typename Result>
+struct Plus<Argument1, Argument2, Result, IS_SCALAR, IS_SCALAR>
+{
+  static void calculate(
+    Argument1 const& argument1,
+    Argument2 const& argument2,
+    Result& result)
+  {
+    result = argument1 + argument2;
+  }
+};
+
+
+
+template<
+  typename Argument1,
+  typename Argument2,
+  typename Result>
+struct Plus<Argument1, Argument2, Result, IS_SCALAR, IS_NOT_SCALAR>
+{
+  static void calculate(
+    Argument1 const& argument1,
+    Argument2 const& argument2,
+    Result& result)
+  {
+    // typedef typename 
+    typename boost::range_const_iterator<Argument2>::type argument2It =
+      boost::begin(argument2);
+    typename boost::range_const_iterator<Argument2>::type const end2 =
+      boost::end(argument2);
+    typename boost::range_iterator<Result>::type resultIt =
+      boost::begin(result);
+
+    for(; argument2It != end2; ++argument2It, ++resultIt) {
+      *resultIt = argument1 + *argument2It;
+    }
+  }
+};
+
+
+
+template<
+  typename Argument1,
+  typename Argument2,
+  typename Result>
+struct Plus<Argument1, Argument2, Result, IS_NOT_SCALAR, IS_SCALAR>
+{
+  static void calculate(
+    Argument1 const& argument1,
+    Argument2 const& argument2,
+    Result& result)
+  {
+    // typedef typename 
+    typename boost::range_const_iterator<Argument1>::type argument1It =
+      boost::begin(argument1);
+    typename boost::range_const_iterator<Argument1>::type const end1 =
+      boost::end(argument1);
+    typename boost::range_iterator<Result>::type resultIt =
+      boost::begin(result);
+
+    for(; argument1It != end1; ++argument1It, ++resultIt) {
+      *resultIt = *argument1It + argument2;
+    }
+  }
+};
+
+
+
+
+template<
+  typename Argument1,
+  typename Argument2,
+  typename Result>
+struct Plus<Argument1, Argument2, Result, IS_NOT_SCALAR, IS_NOT_SCALAR>
+{
+  static void calculate(
+    Argument1 const& argument1,
+    Argument2 const& argument2,
+    Result& result)
+  {
+    // typedef typename 
+    typename boost::range_const_iterator<Argument1>::type argument1It =
+      boost::begin(argument1);
+    typename boost::range_const_iterator<Argument2>::type argument2It =
+      boost::begin(argument2);
+    typename boost::range_const_iterator<Argument1>::type const end1 =
+      boost::end(argument1);
+    typename boost::range_iterator<Result>::type resultIt =
+      boost::begin(result);
+
+    for(; argument1It != end1; ++argument1It, ++argument2It, ++resultIt) {
+      *resultIt = *argument1It + *argument2It;
+    }
+  }
+};
 
 } // Namespace detail
 
 
 
-template<class Argument1, class Argument2, class Result>
+//!
+/*!
+  \tparam    .
+  \param     .
+  \return    .
+  \exception .
+  \warning   .
+  \sa        .
+*/
+template<
+  typename Argument1,
+  typename Argument2,
+  typename Result>
 inline void plus(
   Argument1 const& argument1,
   Argument2 const& argument2,
@@ -26,7 +186,17 @@ inline void plus(
   //   Use interface of InputRaster concept.
   // Order the arguments: Scalar, Raster, to limit the amount of overloads to
   // create.
-  result = argument1 + argument2;
+
+  detail::Plus<Argument1, Argument2, Result,
+    detail::ArgumentTraits<Argument1>::isScalar,
+    detail::ArgumentTraits<Argument2>::isScalar>::calculate(argument1,
+      argument2, result);
+
+  // Iteration must be handled per argument.
+  // We must end up with pieces of code that contain the loop. At that level
+  // we can decide how to iterate (1D array, 2D array, vector, Raster, ...).
+  // Treat scalars and arrays special. The rest must adhere to the Scalar and
+  // Raster concept.
 }
 
 } // namespace algorithm
--- Sources/Ranally/Algorithm/PlusTest.cc
+++ Sources/Ranally/Algorithm/PlusTest.cc
@@ -1,9 +1,40 @@
 #include "Ranally/Algorithm/PlusTest.h"
 
+#include <boost/array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/test/test_tools.hpp>
 #include <boost/test/unit_test_suite.hpp>
 #include "Ranally/Algorithm/Plus.h"
+
+
+
+namespace {
+
+template<
+  typename value_type,
+  size_t nr_rows,
+  size_t nr_cols>
+class Raster
+{
+private:
+
+  boost::array<value_type, nr_rows * nr_cols> _cells;
+
+public:
+
+  Raster()
+  {
+  }
+
+  // Raster(
+  //   value_type initialValue)
+  //   : _cells(initialValue)
+  // {
+  // }
+
+};
+
+} // Anonymous namespace
 
 
 
@@ -37,54 +68,95 @@ void PlusTest::testArgumentAndResultTypes()
     BOOST_CHECK_EQUAL(result, 7);
   }
 
-  // // int + int[2] -> int[2]
-  // {
-  //   int argument1 = 3;
-  //   int argument2[2] = {4, 5};
-  //   int result[2] = {0, 0};
-  //   ranally::algorithm::plus(argument1, argument2, result);
-  //   BOOST_CHECK_EQUAL(result[0], 7);
-  //   BOOST_CHECK_EQUAL(result[1], 8);
-  // }
+  // int + int[2] -> int[2]
+  {
+    int argument1 = 3;
+    int argument2[2] = {4, 5};
+    int result[2] = {0, 0};
+    ranally::algorithm::plus(argument1, argument2, result);
+    BOOST_CHECK_EQUAL(result[0], 7);
+    BOOST_CHECK_EQUAL(result[1], 8);
+  }
 
-  // // int[2] + int -> int[2]
+  // int[2] + int -> int[2]
+  {
+    int argument1[2] = {3, 4};
+    int argument2 = 4;
+    int result[2] = {0, 0};
+    ranally::algorithm::plus(argument1, argument2, result);
+    BOOST_CHECK_EQUAL(result[0], 7);
+    BOOST_CHECK_EQUAL(result[1], 8);
+  }
+
+  // int[2] + int[2] -> int[2]
+  {
+    int argument1[2] = {3, 4};
+    int argument2[2] = {4, 5};
+    int result[2] = {0, 0};
+    ranally::algorithm::plus(argument1, argument2, result);
+    BOOST_CHECK_EQUAL(result[0], 7);
+    BOOST_CHECK_EQUAL(result[1], 9);
+  }
+
+  // int + boost::array<int, 2> -> int[2]
+  {
+    int argument1 = 3;
+    boost::array<int, 2> argument2;
+    argument2[0] = 4;
+    argument2[1] = 5;
+    int result[2] = {0, 0};
+    ranally::algorithm::plus(argument1, argument2, result);
+    BOOST_CHECK_EQUAL(result[0], 7);
+    BOOST_CHECK_EQUAL(result[1], 8);
+  }
+
+  // int[2] + boost::array<int, 2> -> std::vector<int>(2, 0)
+  {
+    int argument1[2] = {3, 4};
+    boost::array<int, 2> argument2;
+    argument2[0] = 4;
+    argument2[1] = 5;
+    std::vector<int> result(2, 0);
+    ranally::algorithm::plus(argument1, argument2, result);
+    BOOST_CHECK_EQUAL(result[0], 7);
+    BOOST_CHECK_EQUAL(result[1], 9);
+  }
+
+  // int[2] + int -> boost::array<int, 2>
+  {
+    int argument1[2] = {3, 4};
+    int argument2 = 4;
+    int result[2] = {0, 0};
+    ranally::algorithm::plus(argument1, argument2, result);
+    BOOST_CHECK_EQUAL(result[0], 7);
+    BOOST_CHECK_EQUAL(result[1], 8);
+  }
+
+  // Raster + Raster -> Raster
+  {
+    Raster<int, 1, 2> argument1;
+    // argument1.set<0, 0>(3);
+    // argument1.set<0, 1>(4);
+    // Raster<int, 1, 2> argument2;
+    // argument2.set<0, 0>(4);
+    // argument2.set<0, 1>(5);
+    // Raster<int, 2, 3> result(0);
+    // ranally::algorithm::plus(argument1, argument2, result);
+    // BOOST_CHECK_EQUAL(result.get<0, 0>(), 7);
+    // BOOST_CHECK_EQUAL(result.get<0, 1>(), 8);
+  }
+
+  // // int[1][2] + int -> int[2]
   // {
-  //   int argument1[2] = {3, 4};
+  //   int argument1[1][2];
+  //   argument1[0][0] = 3;
+  //   argument1[0][1] = 4;
   //   int argument2 = 4;
   //   int result[2] = {0, 0};
   //   ranally::algorithm::plus(argument1, argument2, result);
-  //   BOOST_CHECK_EQUAL(result[0], 7);
-  //   BOOST_CHECK_EQUAL(result[1], 8);
-  // }
-
-  // // int[2] + int[2] -> int[2]
-  // {
-  //   int argument1[2] = {3, 4};
-  //   int argument2[2] = {4, 5};
-  //   int result[2] = {0, 0};
-  //   ranally::algorithm::plus(argument1, argument2, result);
-  //   BOOST_CHECK_EQUAL(result[0], 7);
-  //   BOOST_CHECK_EQUAL(result[1], 9);
-  // }
-
-  // // int + boost::array<int, 2> -> int[2]
-  // {
-  //   int argument1 = 3;
-  //   boost::array<int, 2> = {4, 5};
-  //   int result[2] = {0, 0};
-  //   ranally::algorithm::plus(argument1, argument2, result);
-  //   BOOST_CHECK_EQUAL(result[0], 7);
-  //   BOOST_CHECK_EQUAL(result[1], 8);
-  // }
-
-  // // int[2] + int -> boost::array<int, 2>
-  // {
-  //   int argument1[2] = {3, 4};
-  //   int argument2 = 4;
-  //   int result[2] = {0, 0};
-  //   ranally::algorithm::plus(argument1, argument2, result);
-  //   BOOST_CHECK_EQUAL(result[0], 7);
-  //   BOOST_CHECK_EQUAL(result[1], 8);
+  //   // BOOST_CHECK_EQUAL(result[0], 7);
+  //   // BOOST_CHECK_EQUAL(result[1], 8);
   // }
 }
+
 
