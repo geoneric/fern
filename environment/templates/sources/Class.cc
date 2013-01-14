95251aed48648dbc27dcf3f93153882447932135
Kor de Jong <kor@jemig.eu>
2013-01-14 16:03:04 +0100

    Refactoring

--- environment/templates/sources/Class.cc
+++ environment/templates/sources/Class.cc
@@ -3,13 +3,13 @@
 
 namespace ranally {
 
-Class::Class()
-{
-}
-
-
-Class::~Class()
-{
-}
+// Class::Class()
+// {
+// }
+// 
+// 
+// Class::~Class()
+// {
+// }
 
 } // namespace ranally
--- environment/templates/sources/Class.h
+++ environment/templates/sources/Class.h
@@ -35,5 +35,3 @@ private:
 };
 
 } // namespace ranally
-
-#endif
