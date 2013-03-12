64aaeb958c1ebe5b46e60223d23332e7fb527b50
Kor de Jong <kor@jemig.eu>
2013-03-12 18:54:09 +0100

    Added comments

--- sources/ranally/language/execute_visitor.cc
+++ sources/ranally/language/execute_visitor.cc
@@ -8,8 +8,15 @@ void ExecuteVisitor::Visit(
     OperationVertex& vertex)
 {
     // TODO Execute operation, given the arguments.
-    // 
+    // TODO First evaluate the arguments.
+    // TODO Where to put the results?
 
+    // TODO First print what needs to be done.
+    // TODO Then think about how to do it.
+    //      Maybe first execute, and afterwards think about where to store
+    //      the values.
+    //      Where to store the result values is related to where to store the
+    //      argument values
     std::cout << "execute: " << vertex.name() << std::endl;
 }
 
