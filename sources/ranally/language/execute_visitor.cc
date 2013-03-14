d6c7cec4ef123241a2d0cab616c2c266aaa3fa25
Kor de Jong <kor@jemig.eu>
2013-03-14 08:31:58 +0100

    Added comments

--- sources/ranally/language/execute_visitor.cc
+++ sources/ranally/language/execute_visitor.cc
@@ -17,6 +17,17 @@ void ExecuteVisitor::Visit(
     //      the values.
     //      Where to store the result values is related to where to store the
     //      argument values
+
+    // Maybe create a hierarchy of execution blocks that share the same
+    // base class, but are templated on the argument types, result types and
+    // operation type. (It also includes control flow blocks.) Each execution
+    // block is capable of returning its results in the correct types. Maybe
+    // each execution block is a functor.
+    // Arguments of operations then become execution blocks that can execute
+    // themselves and return a result.
+    // Can we use this idea for a compiler too?
+
+
     std::cout << "execute: " << vertex.name() << std::endl;
 }
 
