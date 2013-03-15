27429b6a1b531f79c154bb6042cb3622f989b817
Kor de Jong <kor@jemig.eu>
2013-03-15 09:31:51 +0100

    Wip, comments

--- sources/ranally/language/execute_visitor.cc
+++ sources/ranally/language/execute_visitor.cc
@@ -4,11 +4,48 @@
 
 namespace ranally {
 
-#define VISIT_NUMBER_VERTEX()                                                  \
-void AnnotateVisitor::Visit(                                                   \
+void ExecuteVisitor::Visit(
+    AssignmentVertex& /* vertex */)
+{
+    // Let the expression execute itself.
+    // Store the result in a scoped symbol table for later reference.
+    // Update scope at correct moments in other visit functions.
+
+    // _symbol_table.add_value(vertex.target()->name(), value);
+}
+
+
+
+void ExecuteVisitor::Visit(
+    IfVertex& /* vertex */)
+{
+    // Evaluate condition.
+    // TODO
+}
+
+
+void ExecuteVisitor::Visit(
+    WhileVertex& /* vertex */)
+{
+    // Evaluate condition.
+    // TODO
+}
+
+
+void ExecuteVisitor::Visit(
+    NameVertex& /* vertex */)
+{
+    // Retrieve the value from the symbol table and push it onto the stack.
+    // _stack.push(_symbol_table.value(vertex.name()));
+}
+
+
+#define VISIT_NUMBER_VERTEX(                                                   \
+    type)                                                                      \
+void ExecuteVisitor::Visit(                                                    \
     NumberVertex<type>& vertex)                                                \
 {                                                                              \
-    std::cout << "Push number on stack" << std::endl                           \
+    _stack.push<type>(vertex.value());                                         \
 }
 
 VISIT_NUMBER_VERTEX(int8_t  )
@@ -28,17 +65,6 @@ VISIT_NUMBER_VERTEX(double  )
 void ExecuteVisitor::Visit(
     OperationVertex& vertex)
 {
-    // TODO Execute operation, given the arguments.
-    // TODO First evaluate the arguments.
-    // TODO Where to put the results?
-
-    // TODO First print what needs to be done.
-    // TODO Then think about how to do it.
-    //      Maybe first execute, and afterwards think about where to store
-    //      the values.
-    //      Where to store the result values is related to where to store the
-    //      argument values
-
     // Maybe create a hierarchy of execution blocks that share the same
     // base class, but are templated on the argument types, result types and
     // operation type. (It also includes control flow blocks.) Each execution
@@ -48,9 +74,48 @@ void ExecuteVisitor::Visit(
     // themselves and return a result.
     // Can we use this idea for a compiler too?
 
-    // TODO Create stack with operand values. boost::any
+    // Let the argument expressions execute themselves. The resulting values
+    // end up at the top of the stack.
+    visit_expressions(vertex.expressions());
 
-    std::cout << "execute: " << vertex.name() << std::endl;
+    Operation const& operation(*vertex.operation());
+    assert(_stack.size() >= operation.arity());
+
+    // We have everything we need: operation, arguments.
+    // Given the properties of the operation:
+    // TODO Try one out, create a test and see how it works.
+
+    if(operation.name() == "Add") {
+        assert(operation.arity() == 2);
+        assert(vertex.data_type(0) == DataTypes::SCALAR);
+        assert(vertex.data_type(1) == DataTypes::SCALAR);
+        assert(vertex.value_type(0) == ValueTypes::FLOAT64);
+        assert(vertex.value_type(1) == ValueTypes::FLOAT64);
+
+        double result = _stack.top<double>(); _stack.pop();
+        result += _stack.top<double>(); _stack.pop();
+        _stack.push(result);
+    }
+}
+
+
+void ExecuteVisitor::Visit(
+    StringVertex& /* vertex */)
+{
+    // Read attribute value from dataset and store on stack.
+    // _stack.push(read(vertex.value());
+}
+
+
+void ExecuteVisitor::Visit(
+    SubscriptVertex& /* vertex */)
+{
+    // Evaluate expression and determine value.
+    // The selection expression must be evaluated given/on the value.
+    // We need a set of operations that make selections.
+    // Evaluating selection expressions requires a different executor. The
+    // expressions have a different meaning in the context of subscripts.
+    // TODO
 }
 
 } // namespace ranally
--- sources/ranally/language/execute_visitor.h
+++ sources/ranally/language/execute_visitor.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "ranally/core/stack.h"
 #include "ranally/language/visitor.h"
 
 
@@ -7,6 +8,19 @@ namespace ranally {
 //! short_description_HORRIBLE_LONG_STRING_TO_NOTICE_THAT_IT_SHOULD_BE_REPLACED
 /*!
   longer_description_HORRIBLE_LONG_STRING_TO_NOTICE_THAT_IT_SHOULD_BE_REPLACED
+
+  Encapsulate executable stuff in an Executor instance (script, function,
+  expression). Each Executor accepts argument values and returns result
+  values.
+
+  A script is an anonymous function. The undefined variables are the
+  script's arguments, which need to satisfied by some other means. If this
+  doesn't happen, then they are really undefined.
+
+  We need a Context that has a symbol table with currently visible variables
+  and their values. Such an instance must be passed into the Executors. That's
+  how they get at values. Each Executor can store local variables locally.
+  They may update global variables in the Context's symbol table.
 
   \sa        .
 */
@@ -32,6 +46,15 @@ public:
 
 private:
 
+    //! Stack with values that are passed in and out of expressions.
+    Stack          _stack;
+
+    void           Visit               (AssignmentVertex& vertex);
+
+    void           Visit               (IfVertex& vertex);
+
+    void           Visit               (NameVertex& vertex);
+
     void           Visit               (NumberVertex<int8_t>& vertex);
 
     void           Visit               (NumberVertex<int16_t>& vertex);
@@ -53,6 +76,12 @@ private:
     void           Visit               (NumberVertex<double>& vertex);
 
     void           Visit               (OperationVertex& vertex);
+
+    void           Visit               (StringVertex& vertex);
+
+    void           Visit               (SubscriptVertex& vertex);
+
+    void           Visit               (WhileVertex& vertex);
 
 };
 
