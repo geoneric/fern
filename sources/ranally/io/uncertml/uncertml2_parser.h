4160d60543d207c10b576136a8bc4644c3614e2b
Kor de Jong <kor@jemig.eu>
2013-04-25 10:41:50 +0200

    Busy with uncertml parser

---
+++ sources/ranally/io/uncertml/uncertml2_parser.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <memory>
+// #include "ranally/core/string.h"
+
+
+namespace ranally {
+
+//! TODO
+/*!
+   TODO
+*/
+class UncertML2Parser
+{
+
+public:
+
+                   UncertML2Parser     ()=default;
+
+                   ~UncertML2Parser    ()=default;
+
+                   UncertML2Parser     (UncertML2Parser&&)=delete;
+
+    UncertML2Parser& operator=         (UncertML2Parser&&)=delete;
+
+                   UncertML2Parser     (UncertML2Parser const&)=delete;
+
+    UncertML2Parser& operator=         (UncertML2Parser const&)=delete;
+
+    // std::shared_ptr<ScriptVertex> parse_string(
+    //                                     String const& xml) const;
+
+private:
+
+    // std::shared_ptr<ScriptVertex> parse(std::istream& stream) const;
+
+};
+
+} // namespace ranally
