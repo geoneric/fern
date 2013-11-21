60d9fcab98923faa918a719f547e12487bc975f8
Kor de Jong <k.dejong@geoneric.eu>
2013-11-21 14:11:02 +0100

    Added fixup_bundle. Added test_model_to_executable.sh

---
+++ environment/scripts/test_model_to_executable.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+model_basename=bla
+model_pathname=/tmp/$model_basename.mod
+install_prefix=/tmp/$model_basename
+
+make -C $OBJECTS/fern all
+echo "a = abs(b)" > $model_pathname
+rm -fr $install_prefix
+
+export CMAKE_MODULE_PATH="$CMAKE_MODULE_PATH;$FERN/environment/cmake"
+export CXXFLAGS="-I$FERN/sources"
+export LDFLAGS="-L$OBJECTS/fern/bin"
+model_to_executable.py $model_pathname $install_prefix
+
+export LD_LIBRARY_PATH=$install_prefix/bin
+$install_prefix/bin/$model_basename --help
