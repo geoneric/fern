54c197d3776a98fad9d20097c9ddc0c473d64407
Kor de Jong <k.dejong@geoneric.eu>
2014-05-16 15:11:05 +0200

    Refactoring CMake scripts

--- environment/cmake/FernMacros.cmake
+++ environment/cmake/FernMacros.cmake
@@ -22,6 +22,30 @@ MACRO(ADD_PARSER_GENERATION_COMMAND
 ENDMACRO()
 
 
+# Targets can be added conditionally. When Fern components are selected during
+# configuration, certain variables are set that tell us whether or not to
+# include a target or group of targets. See FernConfiguration.cmake.
+# TARGET_NAME   : Name of target, snake_case.
+# DIRECTORY_NAME: Name of subdirectory containing the target.
+MACRO(ADD_TARGET_CONDITIONALLY
+        TARGET_NAME
+        DIRECTORY_NAME)
+    # Determine name of variable to check.
+    STRING(TOUPPER ${TARGET_NAME} VARIABLE_NAME)
+    SET(VARIABLE_NAME "FERN_FERN_${VARIABLE_NAME}_REQUIRED")
+
+    # Make sure it is defined.
+    IF(NOT DEFINED ${VARIABLE_NAME})
+        MESSAGE(SEND_ERROR "Variable ${VARIABLE_NAME} is not defined")
+    ENDIF()
+
+    # Evaluate the variable and include target if result is true.
+    IF(${VARIABLE_NAME})
+        ADD_SUBDIRECTORY(${DIRECTORY_NAME})
+    ENDIF()
+ENDMACRO()
+
+
 # Create a static library and an object library.
 # BASENAME: Name of static library to create. The object library will be 
 #           named ${BASENAME}_objects.
