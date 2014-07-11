45dcfb1745e05a0ac7aced52469e312ac208bd77
Kor de Jong <k.dejong@geoneric.eu>
2014-07-11 14:21:45 +0200

    Busy refactoring count

--- sources/fern/algorithm/statistic/detail/count.h
+++ sources/fern/algorithm/statistic/detail/count.h
@@ -3,244 +3,278 @@
 #include "fern/core/argument_categories.h"
 #include "fern/core/argument_traits.h"
 #include "fern/core/collection_traits.h"
+#include "fern/core/thread_client.h"
 #include "fern/algorithm/core/index_ranges.h"
 
 
 namespace fern {
 namespace count {
 namespace detail {
-namespace dispatch {
 
-template<class Values, class Result,
+template<
     class InputNoDataPolicy,
     class OutputNoDataPolicy,
-    class ArrayCollectionCategory>
-class Count
+    class Value,
+    class Result>
+void operation_0d(
+    InputNoDataPolicy const& input_no_data_policy,
+    OutputNoDataPolicy& output_no_data_policy,
+    Value const& values,
+    value_type<Value> const& value,
+    Result& result)
 {
-};
+    if(!input_no_data_policy.is_no_data()) {
+        fern::get(result) = fern::get(values) == fern::get(value) ? 1 : 0;
+    }
+    else {
+        output_no_data_policy.mark_as_no_data();
+    }
+}
 
 
-template<class Values, class Result,
+template<
     class InputNoDataPolicy,
-    class OutputNoDataPolicy>
-class Count<Values, Result,
-        InputNoDataPolicy,
-        OutputNoDataPolicy,
-        array_0d_tag>:
-
-    public InputNoDataPolicy,
-    public OutputNoDataPolicy
-
+    class OutputNoDataPolicy,
+    class Value,
+    class Result>
+void operation_1d(
+    InputNoDataPolicy const& input_no_data_policy,
+    OutputNoDataPolicy& output_no_data_policy,
+    IndexRanges<1> const& index_ranges,
+    Value const& values,
+    value_type<Value> const& value,
+    Result& result)
 {
 
-public:
+    size_t const begin = index_ranges[0].begin();
+    size_t const end = index_ranges[0].end();
+    bool data_seen{false};
 
-    Count()
-        : InputNoDataPolicy(),
-          OutputNoDataPolicy()
-    {
-    }
+    if(begin < end) {
 
-    Count(
-        InputNoDataPolicy&& input_no_data_policy,
-        OutputNoDataPolicy&& output_no_data_policy)
-        : InputNoDataPolicy(std::forward<InputNoDataPolicy>(
-              input_no_data_policy)),
-          OutputNoDataPolicy(std::forward<OutputNoDataPolicy>(
-              output_no_data_policy))
-    {
-    }
+        value_type<Result>& count = fern::get(result);
+        count = 0;
 
-    // 0d array
-    inline void calculate(
-        Values const& values,
-        typename ArgumentTraits<Values>::value_type const& value,
-        Result& result)
-    {
-        using INDP = InputNoDataPolicy;
-        using ONDP = OutputNoDataPolicy;
+        for(size_t i = begin; i < end; ++i) {
 
-        if(!INDP::is_no_data()) {
-            fern::get(result) = fern::get(values) == fern::get(value) ? 1 : 0;
-        }
-        else {
-            ONDP::mark_as_no_data();
+            if(!input_no_data_policy.is_no_data(i)) {
+
+                count += fern::get(values, i) == value ? 1 : 0;
+                data_seen = true;
+            }
         }
     }
 
-};
+    if(!data_seen) {
+        output_no_data_policy.mark_as_no_data();
+    }
+}
 
 
-template<class Values, class Result,
+template<
     class InputNoDataPolicy,
-    class OutputNoDataPolicy>
-class Count<Values, Result,
-        InputNoDataPolicy,
-        OutputNoDataPolicy,
-        array_1d_tag>:
-
-    public InputNoDataPolicy,
-    public OutputNoDataPolicy
-
+    class OutputNoDataPolicy,
+    class Value,
+    class Result>
+void operation_2d(
+    InputNoDataPolicy const& input_no_data_policy,
+    OutputNoDataPolicy& output_no_data_policy,
+    IndexRanges<2> const& index_ranges,
+    Value const& values,
+    value_type<Value> const& value,
+    Result& result)
 {
+    size_t const begin1 = index_ranges[0].begin();
+    size_t const end1 = index_ranges[0].end();
+    size_t const begin2 = index_ranges[1].begin();
+    size_t const end2 = index_ranges[1].end();
+    bool data_seen{false};
 
-public:
+    if(begin1 < end1 && begin2 < end2) {
 
-    Count()
-        : InputNoDataPolicy(),
-          OutputNoDataPolicy()
-    {
-    }
+        value_type<Result>& count = fern::get(result);
+        count = 0;
 
-    Count(
-        InputNoDataPolicy&& input_no_data_policy,
-        OutputNoDataPolicy&& output_no_data_policy)
-        : InputNoDataPolicy(std::forward<InputNoDataPolicy>(
-              input_no_data_policy)),
-          OutputNoDataPolicy(std::forward<OutputNoDataPolicy>(
-              output_no_data_policy))
-    {
-    }
+        for(size_t i = begin1; i < end1; ++i) {
+            for(size_t j = begin2; j < end2; ++j) {
 
-    // 1d array
-    inline void calculate(
-        Values const& values,
-        typename ArgumentTraits<Values>::value_type const& value,
-        Result& result)
-    {
-        size_t const size = fern::size(values);
+                if(!input_no_data_policy.is_no_data(i, j)) {
 
-        auto ranges = IndexRanges<1>{
-            IndexRange(0, size)
-        };
-
-        calculate(ranges, values, value, result);
-    }
-
-    template<
-        class Indices>
-    inline void calculate(
-        Indices const& indices,
-        Values const& values,
-        typename ArgumentTraits<Values>::value_type const& value,
-        Result& result)
-    {
-        size_t const begin = indices[0].begin();
-        size_t const end = indices[0].end();
-        bool data_seen{false};
-
-        if(begin < end) {
-
-            typename ArgumentTraits<Result>::value_type& count =
-                fern::get(result);
-            count = 0;
-
-            for(size_t i = begin; i < end; ++i) {
-
-                if(!InputNoDataPolicy::is_no_data(i)) {
-
-                    count += fern::get(values, i) == value ? 1 : 0;
+                    count += fern::get(values, i, j) == value ? 1 : 0;
                     data_seen = true;
                 }
             }
         }
+    }
 
-        if(!data_seen) {
-            OutputNoDataPolicy::mark_as_no_data();
-        }
+    if(!data_seen) {
+        output_no_data_policy.mark_as_no_data();
+    }
+}
+
+
+namespace dispatch {
+
+template<
+    class ExecutionPolicy,
+    class ArrayCollectionCategory>
+struct Count
+{
+};
+
+
+template<
+    class ExecutionPolicy>
+struct Count<
+    ExecutionPolicy,
+    array_0d_tag>
+{
+
+    template<
+        class InputNoDataPolicy,
+        class OutputNoDataPolicy,
+        class Value,
+        class Result>
+    static void apply(
+        InputNoDataPolicy const& input_no_data_policy,
+        OutputNoDataPolicy& output_no_data_policy,
+        ExecutionPolicy const& /* execution_policy */,
+        Value const& values,
+        value_type<Value> const& value,
+        Result& result)
+    {
+        operation_0d<>(input_no_data_policy, output_no_data_policy,
+            values, value, result);
     }
 
 };
 
 
-template<class Values, class Result,
-    class InputNoDataPolicy,
-    class OutputNoDataPolicy>
-class Count<Values, Result,
-        InputNoDataPolicy,
-        OutputNoDataPolicy,
-        array_2d_tag>:
-
-    public InputNoDataPolicy,
-    public OutputNoDataPolicy
-
+template<>
+struct Count<
+    SequentialExecutionPolicy,
+    array_1d_tag>
 {
 
-public:
-
-    Count()
-        : InputNoDataPolicy(),
-          OutputNoDataPolicy()
-    {
-    }
-
-    Count(
-        InputNoDataPolicy&& input_no_data_policy,
-        OutputNoDataPolicy&& output_no_data_policy)
-        : InputNoDataPolicy(std::forward<InputNoDataPolicy>(
-              input_no_data_policy)),
-          OutputNoDataPolicy(std::forward<OutputNoDataPolicy>(
-              output_no_data_policy))
-    {
-    }
-
-    // 2d array
-    inline void calculate(
-        Values const& values,
-        typename ArgumentTraits<Values>::value_type const& value,
+    template<
+        class InputNoDataPolicy,
+        class OutputNoDataPolicy,
+        class Value,
+        class Result>
+    static void apply(
+        InputNoDataPolicy const& input_no_data_policy,
+        OutputNoDataPolicy& output_no_data_policy,
+        SequentialExecutionPolicy const& /* execution_policy */,
+        Value const& values,
+        value_type<Value> const& value,
         Result& result)
     {
-        size_t const size1 = fern::size(values, 0);
-        size_t const size2 = fern::size(values, 1);
-
-        auto ranges = IndexRanges<2>{
-            IndexRange(0, size1),
-            IndexRange(0, size2)
-        };
-
-        calculate(ranges, values, value, result);
+        operation_1d<>(input_no_data_policy, output_no_data_policy,
+            IndexRanges<1>{IndexRange(0, fern::size(values))},
+            values, value, result);
     }
+
+};
+
+
+template<>
+struct Count<
+    SequentialExecutionPolicy,
+    array_2d_tag>
+{
 
     template<
-        class Indices>
-    inline void calculate(
-        Indices const& indices,
-        Values const& values,
-        typename ArgumentTraits<Values>::value_type const& value,
+        class InputNoDataPolicy,
+        class OutputNoDataPolicy,
+        class Value,
+        class Result>
+    static void apply(
+        InputNoDataPolicy const& input_no_data_policy,
+        OutputNoDataPolicy& output_no_data_policy,
+        SequentialExecutionPolicy const& /* execution_policy */,
+        Value const& values,
+        value_type<Value> const& value,
         Result& result)
     {
-        size_t const begin1 = indices[0].begin();
-        size_t const end1 = indices[0].end();
-        size_t const begin2 = indices[1].begin();
-        size_t const end2 = indices[1].end();
-        bool data_seen{false};
+        operation_2d<>(input_no_data_policy, output_no_data_policy,
+            IndexRanges<2>{
+                IndexRange(0, fern::size(values, 0)),
+                IndexRange(0, fern::size(values, 1))},
+            values, value, result);
+    }
 
-        if(begin1 < end1 && begin2 < end2) {
+};
 
-            typename ArgumentTraits<Result>::value_type& count =
-                fern::get(result);
-            count = 0;
 
-            for(size_t i = begin1; i < end1; ++i) {
-                for(size_t j = begin2; j < end2; ++j) {
+template<>
+struct Count<
+    ParallelExecutionPolicy,
+    array_2d_tag>
+{
 
-                    if(!InputNoDataPolicy::is_no_data(i, j)) {
+    template<
+        class InputNoDataPolicy,
+        class OutputNoDataPolicy,
+        class Value,
+        class Result>
+    static void apply(
+        InputNoDataPolicy const& input_no_data_policy,
+        OutputNoDataPolicy& output_no_data_policy,
+        ParallelExecutionPolicy const& /* execution_policy */,
+        Value const& values,
+        value_type<Value> const& value,
+        Result& result)
+    {
+        ThreadPool& pool(ThreadClient::pool());
+        size_t const size1 = fern::size(values, 0);
+        size_t const size2 = fern::size(values, 1);
+        std::vector<IndexRanges<2>> ranges = index_ranges(pool.size(),
+            size1, size2);
+        std::vector<std::future<void>> futures;
+        futures.reserve(ranges.size());
 
-                        count += fern::get(values, i, j) == value ? 1 : 0;
-                        data_seen = true;
-                    }
-                }
-            }
+        for(auto const& block_range: ranges) {
+            auto function = std::bind(
+                operation_2d<
+                    InputNoDataPolicy, OutputNoDataPolicy,
+                    Value, Result>,
+                std::cref(input_no_data_policy),
+                std::ref(output_no_data_policy), std::cref(block_range),
+                std::cref(values), std::cref(value), std::ref(result));
+            futures.emplace_back(pool.submit(function));
         }
 
-        if(!data_seen) {
-            OutputNoDataPolicy::mark_as_no_data();
+        for(auto& future: futures) {
+            future.get();
         }
     }
 
 };
 
 } // namespace dispatch
+
+
+template<
+    class InputNoDataPolicy,
+    class OutputNoDataPolicy,
+    class ExecutionPolicy,
+    class Value,
+    class Result>
+void count(
+    InputNoDataPolicy const& input_no_data_policy,
+    OutputNoDataPolicy& output_no_data_policy,
+    ExecutionPolicy const& execution_policy,
+    Value const& values,
+    value_type<Value> const& value,
+    Result& result)
+{
+    // Dispatch on the collection category of values and the execution
+    // policy.
+    dispatch::Count<ExecutionPolicy, argument_category<Value>>::apply(
+        input_no_data_policy, output_no_data_policy, execution_policy,
+        values, value, result);
+}
+
 } // namespace detail
 } // namespace count
 } // namespace fern
--- sources/fern/algorithm/statistic/test/count_test.cc
+++ sources/fern/algorithm/statistic/test/count_test.cc
@@ -1,13 +1,10 @@
 #define BOOST_TEST_MODULE fern algorithm statistic count
 #include <boost/test/unit_test.hpp>
 #include "fern/core/constant_traits.h"
-#include "fern/core/typename.h"
 #include "fern/core/vector_traits.h"
 #include "fern/feature/core/array_traits.h"
 #include "fern/feature/core/masked_array_traits.h"
 #include "fern/feature/core/masked_constant_traits.h"
-#include "fern/algorithm/algebra/executor.h"
-#include "fern/algorithm/policy/policies.h"
 #include "fern/algorithm/statistic/count.h"
 
 
@@ -20,20 +17,12 @@ void verify_value(
     R const& result_we_want)
 {
     R result_we_get;
-    fern::statistic::count(array, value, result_we_get);
+    fern::statistic::count(fern::sequential, array, value, result_we_get);
     BOOST_CHECK_EQUAL(result_we_get, result_we_want);
 }
 
 
 BOOST_AUTO_TEST_SUITE(count)
-
-BOOST_AUTO_TEST_CASE(traits)
-{
-    using Count = fern::statistic::Count<int32_t, size_t>;
-    BOOST_CHECK((std::is_same<fern::OperationTraits<Count>::category,
-        fern::local_aggregate_operation_tag>::value));
-}
-
 
 BOOST_AUTO_TEST_CASE(d0_array)
 {
@@ -56,7 +45,7 @@ BOOST_AUTO_TEST_CASE(masked_d0_array)
     result_we_get.value() = 9;
     result_we_get.mask() = false;
     BOOST_CHECK(!constant.mask());
-    fern::statistic::count(constant, 5, result_we_get);
+    fern::statistic::count(fern::sequential, constant, 5, result_we_get);
     BOOST_CHECK(!result_we_get.mask());
     BOOST_CHECK_EQUAL(result_we_get.value(), 1);
 
@@ -66,7 +55,7 @@ BOOST_AUTO_TEST_CASE(masked_d0_array)
     result_we_get.value() = 9;
     result_we_get.mask() = false;
     BOOST_CHECK(constant.mask());
-    fern::statistic::count(constant, 5, result_we_get);
+    fern::statistic::count(fern::sequential, constant, 5, result_we_get);
     BOOST_CHECK(!result_we_get.mask());
     BOOST_CHECK_EQUAL(result_we_get.value(), 1);
 
@@ -81,11 +70,11 @@ BOOST_AUTO_TEST_CASE(masked_d0_array)
     result_we_get.mask() = false;
     BOOST_CHECK(!constant.mask());
     BOOST_CHECK(!result_we_get.mask());
-    fern::statistic::count<fern::MaskedConstant<int32_t>,
-        fern::MaskedConstant<size_t>, InputNoDataPolicy,
-        OutputNoDataPolicy>(
+    OutputNoDataPolicy output_no_data_policy(result_we_get.mask(), true);
+    fern::statistic::count(
             InputNoDataPolicy(constant.mask(), true),
-            OutputNoDataPolicy(result_we_get.mask(), true),
+            output_no_data_policy,
+            fern::sequential,
             constant, 5, result_we_get);
     BOOST_CHECK(!result_we_get.mask());
     BOOST_CHECK_EQUAL(result_we_get.value(), 1);
@@ -97,11 +86,10 @@ BOOST_AUTO_TEST_CASE(masked_d0_array)
     result_we_get.mask() = false;
     BOOST_CHECK(constant.mask());
     BOOST_CHECK(!result_we_get.mask());
-    fern::statistic::count<fern::MaskedConstant<int32_t>,
-        fern::MaskedConstant<size_t>, InputNoDataPolicy,
-        OutputNoDataPolicy>(
+    fern::statistic::count(
             InputNoDataPolicy(constant.mask(), true),
-            OutputNoDataPolicy(result_we_get.mask(), true),
+            output_no_data_policy,
+            fern::sequential,
             constant, 5, result_we_get);
     BOOST_CHECK(result_we_get.mask());
     BOOST_CHECK_EQUAL(result_we_get.value(), 9);
@@ -115,14 +103,14 @@ BOOST_AUTO_TEST_CASE(d1_array)
     // vector
     {
         std::vector<int32_t> array{ 1, 2, 3, 5 };
-        fern::statistic::count(array, 2, result);
+        fern::statistic::count(fern::sequential, array, 2, result);
         BOOST_CHECK_EQUAL(result, 1);
     }
 
     // 1d array
     {
         fern::Array<int32_t, 1> array{ 1, 2, 2, 5 };
-        fern::statistic::count(array, 2, result);
+        fern::statistic::count(fern::sequential, array, 2, result);
         BOOST_CHECK_EQUAL(result, 2);
     }
 
@@ -131,7 +119,7 @@ BOOST_AUTO_TEST_CASE(d1_array)
         // The result value is not touched.
         std::vector<int32_t> array;
         result = 5;
-        fern::statistic::count(array, 2, result);
+        fern::statistic::count(fern::sequential, array, 2, result);
         BOOST_CHECK_EQUAL(result, 5);
     }
 }
@@ -149,7 +137,7 @@ BOOST_AUTO_TEST_CASE(masked_d1_array)
     {
         result.value() = 9;
         result.mask() = false;
-        fern::statistic::count(array, 2, result);
+        fern::statistic::count(fern::sequential, array, 2, result);
         BOOST_CHECK_EQUAL(result.value(), 1);
     }
 
@@ -158,23 +146,20 @@ BOOST_AUTO_TEST_CASE(masked_d1_array)
         array.mask()[2] = true;
         result.value() = 9;
         result.mask() = false;
-        fern::statistic::count<fern::MaskedArray<int32_t, 1>,
-            fern::MaskedConstant<size_t>, InputNoDataPolicy,
-            OutputNoDataPolicy>(
+        OutputNoDataPolicy output_no_data_policy(result.mask(), true);
+        fern::statistic::count(
                 InputNoDataPolicy(array.mask(), true),
-                OutputNoDataPolicy(result.mask(), true),
-                array, 2, result);
+                output_no_data_policy,
+                fern::sequential, array, 2, result);
         BOOST_CHECK(!result.mask());
         BOOST_CHECK_EQUAL(result.value(), 1);
 
         // Mask the whole input. Result must be masked too.
         array.mask_all();
-        fern::statistic::count<fern::MaskedArray<int32_t, 1>,
-            fern::MaskedConstant<size_t>, InputNoDataPolicy,
-            OutputNoDataPolicy>(
+        fern::statistic::count(
                 InputNoDataPolicy(array.mask(), true),
-                OutputNoDataPolicy(result.mask(), true),
-                array, 2, result);
+                output_no_data_policy,
+                fern::sequential, array, 2, result);
         BOOST_CHECK(result.mask());
     }
 
@@ -183,12 +168,11 @@ BOOST_AUTO_TEST_CASE(masked_d1_array)
         fern::MaskedArray<int32_t, 1> empty_array;
         result.value() = 9;
         result.mask() = false;
-        fern::statistic::count<fern::MaskedArray<int32_t, 1>,
-            fern::MaskedConstant<size_t>, InputNoDataPolicy,
-            OutputNoDataPolicy>(
+        OutputNoDataPolicy output_no_data_policy(result.mask(), true);
+        fern::statistic::count(
                 InputNoDataPolicy(empty_array.mask(), true),
-                OutputNoDataPolicy(result.mask(), true),
-                empty_array, 2, result);
+                output_no_data_policy,
+                fern::sequential, empty_array, 2, result);
         BOOST_CHECK(result.mask());
         BOOST_CHECK_EQUAL(result.value(), 9);
     }
@@ -205,7 +189,7 @@ BOOST_AUTO_TEST_CASE(d2_array)
             {  1,  2 }
         };
         size_t result;
-        fern::statistic::count(array, 1, result);
+        fern::statistic::count(fern::sequential, array, 1, result);
         BOOST_CHECK_EQUAL(result, 1);
     }
 }
@@ -222,7 +206,7 @@ BOOST_AUTO_TEST_CASE(masked_d2_array)
     // 2d masked array with non-masking count
     {
         fern::MaskedConstant<size_t> result;
-        fern::statistic::count(array, -2, result);
+        fern::statistic::count(fern::sequential, array, -2, result);
         BOOST_CHECK(!result.mask());
         BOOST_CHECK_EQUAL(result.value(), 1);
     }
@@ -237,23 +221,18 @@ BOOST_AUTO_TEST_CASE(masked_d2_array)
 
         // Count 2's.
         fern::MaskedConstant<size_t> result;
-        fern::statistic::count<fern::MaskedArray<int8_t, 2>,
-            fern::MaskedConstant<size_t>, InputNoDataPolicy,
-            OutputNoDataPolicy>(
-                InputNoDataPolicy(array.mask(), true),
-                OutputNoDataPolicy(result.mask(), true),
-                array, 2, result);
+        OutputNoDataPolicy output_no_data_policy(result.mask(), true);
+        fern::statistic::count(
+                InputNoDataPolicy(array.mask(), true), output_no_data_policy,
+                fern::sequential, array, 2, result);
         BOOST_CHECK(!result.mask());
         BOOST_CHECK_EQUAL(result.value(), 1);
 
         // Count 9's. The one present is not visible, so the result is 0.
         result.value() = 999;
-        fern::statistic::count<fern::MaskedArray<int8_t, 2>,
-            fern::MaskedConstant<size_t>, InputNoDataPolicy,
-            OutputNoDataPolicy>(
-                InputNoDataPolicy(array.mask(), true),
-                OutputNoDataPolicy(result.mask(), true),
-                array, 9, result);
+        fern::statistic::count(
+                InputNoDataPolicy(array.mask(), true), output_no_data_policy,
+                fern::sequential, array, 9, result);
         BOOST_CHECK(!result.mask());
         BOOST_CHECK_EQUAL(result.value(), 0);
 
@@ -261,12 +240,9 @@ BOOST_AUTO_TEST_CASE(masked_d2_array)
         array.mask_all();
         result.mask() = false;
         result.value() = 999;
-        fern::statistic::count<fern::MaskedArray<int8_t, 2>,
-            fern::MaskedConstant<size_t>, InputNoDataPolicy,
-            OutputNoDataPolicy>(
-                InputNoDataPolicy(array.mask(), true),
-                OutputNoDataPolicy(result.mask(), true),
-                array, 9, result);
+        fern::statistic::count(
+                InputNoDataPolicy(array.mask(), true), output_no_data_policy,
+                fern::sequential, array, 9, result);
         BOOST_CHECK(result.mask());
     }
 }
@@ -281,21 +257,20 @@ BOOST_AUTO_TEST_CASE(concurrent)
     fern::Array<int32_t, 2> argument(extents);
     size_t result_we_got;
     size_t result_we_want;
-    fern::statistic::Count<fern::Array<int32_t, 2>, size_t> count;
 
     std::iota(argument.data(), argument.data() + argument.num_elements(), 0);
     result_we_want = 1;
 
     // Serial.
     {
-        fern::serial::execute(count, argument, 5, result_we_got);
+        fern::statistic::count(fern::sequential, argument, 5, result_we_got);
         BOOST_CHECK_EQUAL(result_we_got, result_we_want);
     }
 
     // Concurrent.
     {
         fern::ThreadClient client;
-        fern::concurrent::execute(count, argument, 5, result_we_got);
+        fern::statistic::count(fern::parallel, argument, 5, result_we_got);
         BOOST_CHECK_EQUAL(result_we_got, result_we_want);
     }
 
@@ -303,17 +278,13 @@ BOOST_AUTO_TEST_CASE(concurrent)
         using InputNoDataPolicy = fern::SkipNoData;
         using OutputNoDataPolicy = fern::MarkNoDataByValue<bool>;
         fern::MaskedConstant<size_t> result_we_got;
-        fern::statistic::Count<
-            fern::Array<int32_t, 2>,
-            fern::MaskedConstant<size_t>,
-            InputNoDataPolicy,
-            OutputNoDataPolicy> count(
-                InputNoDataPolicy(),
-                OutputNoDataPolicy(result_we_got.mask(), true));
+        OutputNoDataPolicy output_no_data_policy(result_we_got.mask(), true);
 
         // Verify executor can handle masked result.
         fern::ThreadClient client;
-        fern::concurrent::execute(count, argument, 5, result_we_got);
+        fern::statistic::count(
+            InputNoDataPolicy(), output_no_data_policy,
+            fern::parallel, argument, 5, result_we_got);
     }
 }
 
