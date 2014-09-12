47ace1757f2f34f77af848cd7727710a000ba506
Kor de Jong <k.dejong@geoneric.eu>
2014-09-12 11:11:00 +0200

    Added boolean and algorithm

---
+++ sources/fern/algorithm/algebra/boolean/and.h
@@ -0,0 +1,91 @@
+#pragma once
+#include "fern/core/assert.h"
+#include "fern/algorithm/policy/policies.h"
+#include "fern/algorithm/algebra/boolean/detail/and.h"
+
+
+namespace fern {
+namespace algebra {
+
+//! Determine the boolean and result of \a value1 and \a value2 and write the result to \a result.
+/*!
+    \ingroup       boolean
+    \sa            fern::binary_local_operation.
+
+    The value types of \a value1, \a value2 and \a result must be arithmetic.
+*/
+template<
+    class InputNoDataPolicy,
+    class OutputNoDataPolicy,
+    class ExecutionPolicy,
+    class Value1,
+    class Value2,
+    class Result
+>
+void and_(
+    InputNoDataPolicy const& input_no_data_policy,
+    OutputNoDataPolicy& output_no_data_policy,
+    ExecutionPolicy const& execution_policy,
+    Value1 const& value1,
+    Value2 const& value2,
+    Result& result)
+{
+    FERN_STATIC_ASSERT(std::is_arithmetic, value_type<Value1>)
+    FERN_STATIC_ASSERT(std::is_arithmetic, value_type<Value2>)
+    FERN_STATIC_ASSERT(std::is_arithmetic, value_type<Result>)
+
+    and_::detail::and_<>(input_no_data_policy, output_no_data_policy,
+        execution_policy, value1, value2, result);
+}
+
+
+/*!
+    \ingroup       boolean
+    \overload
+*/
+template<
+    class InputNoDataPolicy,
+    class OutputNoDataPolicy,
+    class ExecutionPolicy,
+    class Value1,
+    class Value2,
+    class Result
+>
+void and_(
+    ExecutionPolicy const& execution_policy,
+    Value1 const& value1,
+    Value2 const& value2,
+    Result& result)
+{
+    OutputNoDataPolicy output_no_data_policy;
+    and_<>(InputNoDataPolicy(), output_no_data_policy, execution_policy,
+        value1, value2, result);
+}
+
+
+/*!
+    \ingroup       boolean
+    \overload
+*/
+template<
+    class ExecutionPolicy,
+    class Value1,
+    class Value2,
+    class Result
+>
+void and_(
+    ExecutionPolicy const& execution_policy,
+    Value1 const& value1,
+    Value2 const& value2,
+    Result& result)
+{
+    using InputNoDataPolicy = SkipNoData;
+    using OutputNoDataPolicy = DontMarkNoData;
+
+    OutputNoDataPolicy output_no_data_policy;
+    and_<>(InputNoDataPolicy(), output_no_data_policy, execution_policy,
+        value1, value2, result);
+}
+
+} // namespace algebra
+} // namespace fern
---
+++ sources/fern/algorithm/algebra/boolean/test/and_test.cc
@@ -0,0 +1,33 @@
+#define BOOST_TEST_MODULE fern algorithm algebra boolean and_
+#include <boost/test/unit_test.hpp>
+#include "fern/core/constant_traits.h"
+#include "fern/core/type_traits.h"
+#include "fern/algorithm/algebra/boolean/and.h"
+
+
+BOOST_AUTO_TEST_SUITE(and_)
+
+template<
+    class Value1,
+    class Value2,
+    class Result>
+void verify_value(
+    Value1 const& value1,
+    Value2 const& value2,
+    Result const& result_we_want)
+{
+    Result result_we_get;
+    fern::algebra::and_(fern::sequential, value1, value2, result_we_get);
+    BOOST_CHECK_EQUAL(result_we_get, result_we_want);
+}
+
+
+BOOST_AUTO_TEST_CASE(algorithm)
+{
+    verify_value<>(true, true, true);
+    verify_value<>(true, false, false);
+    verify_value<>(false, true, false);
+    verify_value<>(false, false, false);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
