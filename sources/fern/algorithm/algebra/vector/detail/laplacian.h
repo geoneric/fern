1ae917967eba78817f030f710fba329d4f56a074
Kor de Jong <k.dejong@geoneric.eu>
2014-09-22 10:03:46 +0200

    Removed header inclusions

--- sources/fern/algorithm/algebra/vector/detail/laplacian.h
+++ sources/fern/algorithm/algebra/vector/detail/laplacian.h
@@ -2,11 +2,8 @@
 #include <utility>
 #include "fern/core/assert.h"
 #include "fern/core/clone.h"
-#include "fern/core/thread_client.h"
 #include "fern/core/value_type.h"
 #include "fern/feature/core/array_traits.h"
-#include "fern/feature/core/masked_array_traits.h"
-#include "fern/feature/core/masked_raster.h"
 #include "fern/algorithm/core/cast.h"
 #include "fern/algorithm/convolution/convolve.h"
 #include "fern/algorithm/convolution/dont_divide_by_weights.h"
