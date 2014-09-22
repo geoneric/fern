fd1fb418086970a6651898938e4d59386d44f436
Kor de Jong <k.dejong@geoneric.eu>
2014-09-22 13:50:04 +0200

    Updated Doxygen handling

--- environment/cmake/DoxygenDoc.cmake
+++ environment/cmake/DoxygenDoc.cmake
@@ -41,26 +41,12 @@ CONFIGURE_FILE(
     ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
 )
 
-# ADD_CUSTOM_COMMAND(
-#     OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/html/index.html
-#     COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-#     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-# )
-
-# This target is *always considered out of date*.
-ADD_CUSTOM_TARGET(cpp_doc ALL
+ADD_CUSTOM_COMMAND(
+    OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/html/index.html
     COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-    # Doesn't work on Windows / mingw32. CMake thinks this results in an error.
-    # 2>&1 | grep --invert-match "QGDict::hashAsciiKey: Invalid null key"
     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-
-    # DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/html/index.html
 )
 
-# ADD_CUSTOM_TARGET(cpp_doc
-#     ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-#     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-#     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-#     COMMENT "Generating API documentation" VERBATIM
-# )
-
+ADD_CUSTOM_TARGET(cpp_doc ALL
+    DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/html/index.html
+)
