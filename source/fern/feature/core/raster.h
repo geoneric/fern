1cf5d780fbf2a1f7ca42a21eca72431029c2ead4
Kor de Jong <k.dejong@geoneric.eu>
2014-10-16 14:19:02 +0200

    Added non-masking raster + traits

---
+++ source/fern/feature/core/raster.h
@@ -0,0 +1,107 @@
+#pragma once
+#include <array>
+#include "fern/feature/core/array.h"
+
+
+namespace fern {
+
+
+//! Class for multidimensional rasters, with a cartesian coordinate system.
+/*!
+    \sa        Array
+
+    Raster extents Array with support for a Cartesian projection
+    coordinate system. A raster is an array which is positioned in a
+    Cartesian coordinate system.
+*/
+template<
+    typename T,
+    size_t nr_dimensions>
+class Raster:
+    public Array<T, nr_dimensions>
+{
+
+public:
+
+    //! Information for transforming cell indices to projection coordinates.
+    /*!
+        For each dimension, this array contains:
+        - Projection coordinate of the border of the first cell.
+        - Size of each cell.
+
+        With this information, cell indices can be translated to projection
+        coordinates, and vice versa.
+
+        For example, in case of a 2D raster, this array can be used like this:
+
+        \code
+        Transformation transformation{{west, cell_width, north, cell_height}};
+        \endcode
+
+        In case of a 2D raster, the cell indices of the upper left cell
+        are 0, 0. The center of this cell has cell indices 0.5, 0.5.
+    */
+    using Transformation = std::array<double, 2 * nr_dimensions>;
+
+    template<size_t nr_ranges>
+                   Raster              (gen_type<nr_ranges> const& sizes,
+                                        Transformation const& transformation);
+
+    template<size_t nr_ranges>
+                   Raster              (gen_type<nr_ranges> const& sizes,
+                                        Transformation const& transformation,
+                                        T const& value);
+
+    Transformation const&
+                   transformation      () const;
+
+private:
+
+    Transformation _transformation;
+
+};
+
+
+template<
+    typename T,
+    size_t nr_dimensions>
+template<
+    size_t nr_ranges>
+inline Raster<T, nr_dimensions>::Raster(
+    gen_type<nr_ranges> const& sizes,
+    Transformation const& transformation)
+
+    : Array<T, nr_dimensions>(sizes),
+      _transformation(transformation)
+
+{
+}
+
+
+template<
+    typename T,
+    size_t nr_dimensions>
+template<
+    size_t nr_ranges>
+inline Raster<T, nr_dimensions>::Raster(
+    gen_type<nr_ranges> const& sizes,
+    Transformation const& transformation,
+    T const& value)
+
+    : Array<T, nr_dimensions>(sizes, value),
+      _transformation(transformation)
+
+{
+}
+
+
+template<
+    typename T,
+    size_t nr_dimensions>
+inline typename Raster<T, nr_dimensions>::Transformation const&
+    Raster<T, nr_dimensions>::transformation() const
+{
+    return _transformation;
+}
+
+} // namespace fern
--- source/fern/feature/core/raster_traits.h
+++ source/fern/feature/core/raster_traits.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstddef>
 #include "fern/core/argument_traits.h"
+#include "fern/feature/core/raster.h"
 
 
 namespace fern {
@@ -34,4 +35,246 @@ RASTER_CATEGORY_TAG(3)
 
 } // namespace dispatch
 } // namespace detail
+
+
+template<
+    typename T,
+    size_t nr_dimensions>
+struct ArgumentTraits<
+    Raster<T, nr_dimensions>>
+{
+
+    using argument_category = typename detail::dispatch::RasterCategoryTag<T,
+        nr_dimensions>::type;
+
+    template<
+        typename U>
+    struct Collection
+    {
+        using type = Raster<U, nr_dimensions>;
+    };
+
+    template<
+        typename U>
+    struct Clone
+    {
+        using type = Raster<U, nr_dimensions>;
+    };
+
+    using value_type = T;
+
+    using reference = T&;
+
+    using const_reference = T const&;
+
+    static bool const is_masking = true;
+
+    static size_t const rank = nr_dimensions;
+
+};
+
+
+template<
+    typename T,
+    size_t nr_dimensions>
+inline size_t size(
+    Raster<T, nr_dimensions> const& raster)
+{
+    return raster.num_elements();
+}
+
+
+template<
+    typename T,
+    size_t nr_dimensions>
+inline size_t size(
+    Raster<T, nr_dimensions> const& raster,
+    size_t dimension)
+{
+    assert(dimension < raster.num_dimensions());
+    return raster.shape()[dimension];
+}
+
+
+template<
+    typename T>
+inline typename ArgumentTraits<Raster<T, 1>>::const_reference get(
+    Raster<T, 1> const& raster,
+    size_t index)
+{
+    assert(index < raster.shape()[0]);
+    // Don't assert this. Depending on the policy used, mask may not be
+    // relevant.
+    // assert(!raster.mask()[index]);
+    return raster[index];
+}
+
+
+template<
+    typename T>
+inline typename ArgumentTraits<Raster<T, 1>>::reference get(
+    Raster<T, 1>& raster,
+    size_t index)
+{
+    assert(index < raster.shape()[0]);
+    // assert(!raster.mask()[index]);
+    return raster[index];
+}
+
+
+template<
+    typename U,
+    typename V>
+inline Raster<U, 1> clone(
+    Raster<V, 1> const& raster)
+{
+    return std::move(Raster<U, 1>(extents[raster.shape()[0]],
+        raster.transformation()));
+}
+
+
+template<
+    typename U,
+    typename V>
+inline Raster<U, 1> clone(
+    Raster<V, 1> const& raster,
+    U const& value)
+{
+    return std::move(Raster<U, 1>(extents[raster.shape()[0]],
+        raster.transformation(),
+        value));
+}
+
+
+template<
+    typename T>
+inline double cell_size(
+    Raster<T, 2> const& raster,
+    size_t index)
+{
+    assert(index == 0 || index == 1);
+    return raster.transformation()[index == 0 ? 1 : 3];
+}
+
+
+template<
+    typename T>
+inline double cell_area(
+    Raster<T, 2> const& raster)
+{
+    return raster.transformation()[1] * raster.transformation()[3];
+}
+
+
+template<
+    typename T>
+inline typename ArgumentTraits<Raster<T, 2>>::const_reference get(
+    Raster<T, 2> const& raster,
+    size_t index1,
+    size_t index2)
+{
+    assert(index1 < raster.shape()[0]);
+    assert(index2 < raster.shape()[1]);
+    // assert(!raster.mask()[index1][index2]);
+    return raster[index1][index2];
+}
+
+
+template<
+    typename T>
+inline typename ArgumentTraits<Raster<T, 2>>::reference get(
+    Raster<T, 2>& raster,
+    size_t index1,
+    size_t index2)
+{
+    assert(index1 < raster.shape()[0]);
+    assert(index2 < raster.shape()[1]);
+    // assert(!raster.mask()[index1][index2]);
+    return raster[index1][index2];
+}
+
+
+template<
+    typename U,
+    typename V>
+inline Raster<U, 2> clone(
+    Raster<V, 2> const& raster)
+{
+    return std::move(Raster<U, 2>(
+        extents[raster.shape()[0]][raster.shape()[1]],
+        raster.transformation()));
+}
+
+
+template<
+    typename U,
+    typename V>
+inline Raster<U, 2> clone(
+    Raster<V, 2> const& raster,
+    U const& value)
+{
+    return std::move(Raster<U, 2>(
+        extents[raster.shape()[0]][raster.shape()[1]],
+        raster.transformation(),
+        value));
+}
+
+
+template<
+    typename T>
+inline typename ArgumentTraits<Raster<T, 3>>::const_reference get(
+    Raster<T, 3> const& raster,
+    size_t index1,
+    size_t index2,
+    size_t index3)
+{
+    assert(index1 < raster.shape()[0]);
+    assert(index2 < raster.shape()[1]);
+    assert(index3 < raster.shape()[2]);
+    // assert(!raster.mask()[index1][index2][index3]);
+    return raster[index1][index2][index3];
+}
+
+
+template<
+    typename T>
+inline typename ArgumentTraits<Raster<T, 3>>::reference get(
+    Raster<T, 3>& raster,
+    size_t index1,
+    size_t index2,
+    size_t index3)
+{
+    assert(index1 < raster.shape()[0]);
+    assert(index2 < raster.shape()[1]);
+    assert(index3 < raster.shape()[2]);
+    // assert(!raster.mask()[index1][index2][index3]);
+    return raster[index1][index2][index3];
+}
+
+
+template<
+    typename U,
+    typename V>
+inline Raster<U, 3> clone(
+    Raster<V, 3> const& raster)
+{
+    return std::move(Raster<U, 3>(
+        extents[raster.shape()[0]][raster.shape()[1],raster.shape()[2]],
+        raster.transformation()));
+}
+
+
+template<
+    typename U,
+    typename V>
+inline Raster<U, 3> clone(
+    Raster<V, 3> const& raster,
+    U const& value)
+{
+    return std::move(Raster<U, 3>(
+        extents[raster.shape()[0]][raster.shape()[1],raster.shape()[2]],
+        raster.transformation(),
+        value));
+}
+
 } // namespace fern
