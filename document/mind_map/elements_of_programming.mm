fc7ad42e7976c5d9275cb747681bbf5d3004e093
Kor de Jong <k.dejong@geoneric.eu>
2014-11-11 17:38:17 +0100

    Added mind map

---
+++ document/mind_map/elements_of_programming.mm
@@ -0,0 +1,17 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1415691105471" ID="ID_503925791" MODIFIED="1415691175346" TEXT="Elements of programming">
+<node CREATED="1415691181618" ID="ID_50592961" MODIFIED="1415691185840" POSITION="right" TEXT="Entity">
+<node CREATED="1415691186809" ID="ID_2657430" MODIFIED="1415691192813" TEXT="Abstract entity">
+<node CREATED="1415691211489" ID="ID_1800035080" MODIFIED="1415691228517" TEXT="Individual thing that is eternal and unchangeable">
+<node CREATED="1415691280193" ID="ID_1979715549" MODIFIED="1415691295957" TEXT="Blue, 13"/>
+</node>
+</node>
+<node CREATED="1415691194472" ID="ID_1261335040" MODIFIED="1415691206293" TEXT="Concrete entity">
+<node CREATED="1415691230905" ID="ID_293860159" MODIFIED="1415691255836" TEXT="Individual thing that comes into and out of existence in space and time">
+<node CREATED="1415691297913" ID="ID_855090914" MODIFIED="1415691315164" TEXT="Socrates, United States of America"/>
+</node>
+</node>
+</node>
+</node>
+</map>
