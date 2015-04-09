c40da74684e94e0cdd9f354779349cfb2607a418
Kor de Jong <k.dejong@geoneric.eu>
2015-04-09 08:42:33 +0200

    Removed tmp files

--- source/fern/python/algorithm/tmp/gdal/overloads.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "fern/python_extension/algorithm/gdal/add_overloads.h"
-#include "fern/python_extension/algorithm/gdal/slope_overloads.h"
--- source/fern/python/algorithm/tmp/numpy/overloads.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "fern/python_extension/algorithm/numpy/add_overloads.h"
-#include "fern/python_extension/algorithm/numpy/sqrt_overloads.h"
