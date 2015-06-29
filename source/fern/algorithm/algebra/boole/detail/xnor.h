33720921ebbd4452a420f55c6c27f08182c81bd1
Kor de Jong <k.dejong@geoneric.eu>
2015-06-29 13:58:25 +0200

    Implement xnor algorithm

---
+++ source/fern/algorithm/algebra/boole/detail/xnor.h
@@ -0,0 +1,83 @@
+// -----------------------------------------------------------------------------
+// Fern © Geoneric
+//
+// This file is part of Geoneric Fern which is available under the terms of
+// the GNU General Public License (GPL), version 2. If you do not want to
+// be bound by the terms of the GPL, you may purchase a proprietary license
+// from Geoneric (http://www.geoneric.eu/contact).
+// -----------------------------------------------------------------------------
+#pragma once
+#include "fern/algorithm/core/binary_local_operation.h"
+#include "fern/algorithm/policy/policies.h"
+
+
+namespace fern {
+namespace algorithm {
+namespace xnor {
+namespace detail {
+
+template<
+    typename Value1,
+    typename Value2>
+struct Algorithm
+{
+
+    template<
+        typename Result>
+    inline void operator()(
+        Value1 const& value1,
+        Value2 const& value2,
+        Result& result) const
+    {
+        result = !value1 == !value2;
+    }
+
+};
+
+
+template<>
+struct Algorithm<
+    bool,
+    bool>
+{
+
+    template<
+        typename Result>
+    inline void operator()(
+        bool const& value1,
+        bool const& value2,
+        Result& result) const
+    {
+        result = value1 == value2;
+    }
+
+};
+
+
+template<
+    typename InputNoDataPolicy,
+    typename OutputNoDataPolicy,
+    typename ExecutionPolicy,
+    typename Value1,
+    typename Value2,
+    typename Result
+>
+void xnor(
+    InputNoDataPolicy const& input_no_data_policy,
+    OutputNoDataPolicy& output_no_data_policy,
+    ExecutionPolicy& execution_policy,
+    Value1 const& value1,
+    Value2 const& value2,
+    Result& result)
+{
+    binary_local_operation<Algorithm,
+        binary::DiscardDomainErrors, binary::DiscardRangeErrors>(
+            input_no_data_policy, output_no_data_policy,
+            execution_policy,
+            value1, value2, result);
+}
+
+} // namespace detail
+} // namespace xnor
+} // namespace algorithm
+} // namespace fern
---
+++ source/fern/algorithm/algebra/boole/test/xnor_test.cc
@@ -0,0 +1,59 @@
+// -----------------------------------------------------------------------------
+// Fern © Geoneric
+//
+// This file is part of Geoneric Fern which is available under the terms of
+// the GNU General Public License (GPL), version 2. If you do not want to
+// be bound by the terms of the GPL, you may purchase a proprietary license
+// from Geoneric (http://www.geoneric.eu/contact).
+// -----------------------------------------------------------------------------
+#define BOOST_TEST_MODULE fern algorithm algebra boole xnor
+#include <boost/test/unit_test.hpp>
+#include "fern/core/data_customization_point/scalar.h"
+#include "fern/core/type_traits.h"
+#include "fern/algorithm/algebra/boole/xnor.h"
+
+
+namespace fa = fern::algorithm;
+
+
+BOOST_AUTO_TEST_SUITE(xnor)
+
+template<
+    class Value1,
+    class Value2,
+    class Result>
+void verify_value(
+    Value1 const& value1,
+    Value2 const& value2,
+    Result const& result_we_want)
+{
+    Result result_we_get;
+    fa::algebra::xnor(fa::sequential, value1, value2, result_we_get);
+    BOOST_CHECK_EQUAL(result_we_get, result_we_want);
+}
+
+
+BOOST_AUTO_TEST_CASE(algorithm)
+{
+    verify_value<>(false, false, true);
+    verify_value<>(false, true, false);
+    verify_value<>(true, false, false);
+    verify_value<>(true, true, true);
+
+    verify_value<>(0, 0, 1);
+    verify_value<>(0, 1, 0);
+    verify_value<>(1, 0, 0);
+    verify_value<>(1, 1, 1);
+
+    verify_value<>( 0,  0,  1);
+    verify_value<>( 0, -1,  0);
+    verify_value<>(-1,  0,  0);
+    verify_value<>(-1, -1,  1);
+
+    verify_value<>( 0.0,  0.0,  1.0);
+    verify_value<>( 0.0, -1.0,  0.0);
+    verify_value<>(-1.0,  0.0,  0.0);
+    verify_value<>(-1.0, -1.0,  1.0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
