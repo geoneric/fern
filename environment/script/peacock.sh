649facfeda2f6a731e6671b37174ea68bdc8faaa
Kor de Jong <k.dejong@geoneric.eu>
2018-06-21 11:12:24 +0200

    Update peacock.sh

--- environment/script/peacock.sh
+++ environment/script/peacock.sh
@@ -38,67 +38,86 @@ function parse_commandline()
 
 function build_peacock()
 {
-    if [[ $OSTYPE == "cygwin" ]]; then
-        options+=("-GUnix Makefiles")
-        options+=("-DCMAKE_MAKE_PROGRAM=mingw32-make")
+    # Don't build any of the support libraries...
+    build_boost=false
+    build_hdf5=false
+    build_netcdf=false
+    build_gdal=false
+    build_hpx=false
+    build_google_benchmark=false
+
+
+    # ... except for these machines
+    hostname=`hostname`
+
+    if [ $hostname == "sonic.geo.uu.nl" ]; then
+        build_boost=true
     fi
 
-    if [[ $OSTYPE == "linux-gnu" ]] && [[ "$CC" == *mingw* ]]; then
-        # Cross-compiling, need toolchain file.
-        options+=("-DCMAKE_TOOLCHAIN_FILE=$source/cmake/toolchain/mingw-linux.cmake")
-    fi
 
     options+=("-Dpeacock_download_dir=$download_dir")
     options+=("-Dpeacock_prefix=$prefix")
-    options+=("-DCMAKE_VERBOSE_MAKEFILE=ON")
+    # options+=("-DCMAKE_VERBOSE_MAKEFILE=ON")
 
 
-    # Boost.
-    options+=("-Dbuild_boost=true")
-    options+=("-Dboost_version=1.57.0")
-    options+=("-Dboost_build_boost_atomic=true")
-    options+=("-Dboost_build_boost_chrono=true")
-    options+=("-Dboost_build_boost_date_time=true")
-    options+=("-Dboost_build_boost_filesystem=true")
-    options+=("-Dboost_build_boost_program_options=true")
-    # Doesn't work icw clang/linux
-    # options+=("-Dboost_build_boost_python=true")
-    options+=("-Dboost_build_boost_regex=true")
-    options+=("-Dboost_build_boost_serialization=true")
-    options+=("-Dboost_build_boost_system=true")
-    options+=("-Dboost_build_boost_test=true")
-    options+=("-Dboost_build_boost_thread=true")
-    options+=("-Dboost_build_boost_timer=true")
+    if [ "$build_boost" = true ]; then
+        options+=("-Dbuild_boost=true")
+        options+=("-Dboost_version=1.57.0")
+        options+=("-Dboost_build_boost_atomic=true")
+        options+=("-Dboost_build_boost_chrono=true")
+        options+=("-Dboost_build_boost_date_time=true")
+        options+=("-Dboost_build_boost_filesystem=true")
+        options+=("-Dboost_build_boost_program_options=true")
+        # Doesn't work icw clang/linux
+        # options+=("-Dboost_build_boost_python=true")
+        options+=("-Dboost_build_boost_regex=true")
+        options+=("-Dboost_build_boost_serialization=true")
+        options+=("-Dboost_build_boost_system=true")
+        options+=("-Dboost_build_boost_test=true")
+        options+=("-Dboost_build_boost_thread=true")
+        options+=("-Dboost_build_boost_timer=true")
+    fi
 
-    # HDF5.
-    # Fern I/O code uses deprecated public API symbols.
-    options+=("-Dbuild_hdf5=true")
-    options+=("-Dhdf5_cpp_lib=false")
-    options+=("-Dhdf5_deprecated_symbols=true")
-    options+=("-Dhdf5_parallel=false")
-    options+=("-Dhdf5_thread_safe=true")
-    options+=("-Dhdf5_version=1.8.14")
 
-    # NetCDF.
-    options+=("-Dbuild_netcdf=true")
-    options+=("-Dnetcdf_version=4.3.3.1")
+    if [ "$build_hdf5" = true ]; then
+        # Fern I/O code uses deprecated public API symbols.
+        options+=("-Dbuild_hdf5=true")
+        options+=("-Dhdf5_cpp_lib=false")
+        options+=("-Dhdf5_deprecated_symbols=true")
+        options+=("-Dhdf5_parallel=false")
+        options+=("-Dhdf5_thread_safe=true")
+        options+=("-Dhdf5_version=1.8.14")
+    fi
 
-    # When building HPX, HDF5 must be build with thread_safe=true.
-    # This, in turn, prevents support for HDF5 C++ support.
-    # HPX.
-    options+=("-Dbuild_hpx=true")
-    options+=("-Dhpx_version=0.9.10")
 
-    # GDAL.
-    options+=("-Dbuild_gdal=true")
-    options+=("-Dgdal_version=1.11.1")
-    options+=("-Dgdal_build_python_package=true")
-    options+=("-Dgdal_with_hdf5=true")
-    options+=("-Dgdal_with_netcdf=true")
+    if [ "$build_netcdf" = true ]; then
+        options+=("-Dbuild_netcdf=true")
+        options+=("-Dnetcdf_version=4.3.3.1")
+    fi
 
-    # Google Benchmark
-    options+=("-Dbuild_google_benchmark=true")
-    options+=("-Dgoogle_benchmark_version=1.3.0")
+
+    if [ "$build_hpx" = true ]; then
+        # When building HPX, HDF5 must be build with thread_safe=true.
+        # This, in turn, prevents support for HDF5 C++ support.
+        # HPX.
+        options+=("-Dbuild_hpx=true")
+        options+=("-Dhpx_version=0.9.10")
+    fi
+
+
+    if [ "$build_gdal" = true ]; then
+        options+=("-Dbuild_gdal=true")
+        options+=("-Dgdal_version=1.11.1")
+        options+=("-Dgdal_build_python_package=true")
+        options+=("-Dgdal_with_hdf5=true")
+        options+=("-Dgdal_with_netcdf=true")
+    fi
+
+
+    if [ "$build_google_benchmark" = true ]; then
+        options+=("-Dbuild_google_benchmark=true")
+        options+=("-Dgoogle_benchmark_version=1.3.0")
+    fi
 
 
     cmake "${options[@]}" $source
